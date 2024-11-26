5273c498243fa00d3f2f5d693c645e45f1e1343a
jaceksanko <jacek.sanko@gmail.com>
2024-11-26 19:49:47 +0100

    Refactor container-components

--- examples/module1/lesson2/container-components/components/SearchTitle.tsx
+++ examples/module1/lesson2/container-components/components/SearchTitle.tsx
@@ -1,5 +1,7 @@
-function SearchTitle() {
-  return <h1 className="text-2xl">Wyszukiwarka postaci Rick and Morty</h1>;
+const TITLE = 'Rick and Morty';
+
+function SearchTitle({ title = TITLE }: { title?: string }) {
+  return <h1 className="text-2xl">{`Wyszukiwarka postaci ${title}`}</h1>;
 }
 
 export default SearchTitle;
