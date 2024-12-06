884c2ca627d1a37678396262c44391228c15a408
jaceksanko <jacek.sanko@gmail.com>
2024-12-06 20:01:39 +0100

    axios-interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,17 +2,20 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: new Date() };
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  const {
+    config: { url, metadata },
+  } = response;
+  const elapsedTime = new Date().getTime() - metadata.startTime.getTime();
+  console.log(`Request to ${url} took ${elapsedTime}ms`);
   return response;
 });
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
+const { data } = await axios.get('/api/data/articles?timeout=3000');
 
-document.querySelector('#data').innerHTML = articles[0].content;
+document.querySelector('#data').innerHTML = data[0].content;
